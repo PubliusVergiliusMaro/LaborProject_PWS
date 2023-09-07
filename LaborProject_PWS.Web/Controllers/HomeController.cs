45007c0fe5a059ef1e470371293af920c2bd177e
Oleg <olegredko45@gmail.com>
2023-09-07 10:09:22 +0300

    Add project files.

---
+++ LaborProject_PWS.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LaborProject_PWS.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LaborProject_PWS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LaborProject_PWS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaborProject_PWS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
