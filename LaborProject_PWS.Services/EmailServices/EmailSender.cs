32c89010129aa588552307cfd52ba47f20a47106
Oleg <olegredko45@gmail.com>
2023-11-11 00:31:34 +0200

    Add EmailSender

---
+++ LaborProject_PWS.Services/EmailServices/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace LaborProject_PWS.Services.EmailServices
+{
+	public class EmailSender : IEmailSender
+	{
+		public async Task SendEmailAsync(string email, string subject, string message)
+		{
+			var apiKey = "SenGrid_API_Key";
+			var client = new SendGridClient(apiKey);
+			var from = new EmailAddress("olegredko45@gmail.com", "Example User");
+			var to = new EmailAddress(email, "Example User");
+			var msg = MailHelper.CreateSingleEmail(from, to, subject, message, $"<strong>{message}</strong>");
+			var response = await client.SendEmailAsync(msg);
+		}
+	}
+}
